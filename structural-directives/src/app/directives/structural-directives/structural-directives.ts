61de12f66339a4eb3c8a7c461e8e865048bbf67b
Anil Kumar <anilkumar.dcttechnology@gmail.com>
2025-07-02 18:45:05 +0530

    NgSwitch Added

--- structural-directives/src/app/directives/structural-directives/structural-directives.ts
+++ structural-directives/src/app/directives/structural-directives/structural-directives.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-structural-directives',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './structural-directives.html',
   styleUrl: './structural-directives.css',
 })
@@ -21,4 +22,5 @@ export class StructuralDirectives {
     'Anil',
     'Anand',
   ];
+  selectedColor: string = 'red';
 }
